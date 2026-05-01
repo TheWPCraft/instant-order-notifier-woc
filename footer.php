0bf755f47529357c962bc62f232916145e367a61
TheWPCraft <thewpcraft@gmail.com>
2026-05-01 14:37:35 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('ABSPATH')) exit;
+?>
+
+<footer class="text-center mt-3">
+    <p class="">
+        Developed with ❤️ by <strong>The WP Craft</strong> | All Rights Reserved</p>
+</footer>
+</main>
